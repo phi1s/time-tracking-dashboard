288407d9dae5121656a3b82df516b088efbc077e
philipp <scnp@zhaw.ch>
2023-01-16 22:50:14 +0100

    add logic

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,40 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+  <link
+    href="https://fonts.googleapis.com/css2?family=Rubik:wght@300;400;500&display=swap"
+    rel="stylesheet"
+  />
+  <TimeTrackingDashboard />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TimeTrackingDashboard from "./components/TimeTrackingDashboard.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    TimeTrackingDashboard,
+  },
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Rubik", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+  color: #ffffff;
+  background-color: #0e1323;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  overflow: hidden;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 </style>
---
+++ src/components/TimeTrackingDashboard.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="container">
+    <div class="overlay">
+      <img src="../assets/images/image-jeremy.png" alt="portrait" />
+      <p>Report for</p>
+      <h1>Jermemy Robson</h1>
+    </div>
+    <div class="menu">
+      <button @click="(timeframe = 'daily'), (description = 'Yesterday')">
+        Daily
+      </button>
+      <button @click="(timeframe = 'weekly'), (description = 'Last Week')">
+        Weekly
+      </button>
+      <button @click="(timeframe = 'monthly'), (description = 'Last Month')">
+        Monthly
+      </button>
+    </div>
+    <div class="categories" v-for="item in items" :key="item.title">
+      <h3>{{ item.title }}</h3>
+      <h2>{{ item.timeframes[timeframe].current }}hrs</h2>
+      <p>{{ description + " " + item.timeframes[timeframe].previous }}hrs</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import data from "../../data.json";
+export default {
+  name: "TimeTrackingDashboard",
+  data() {
+    return {
+      items: data,
+      timeframe: "weekly",
+      description: "Last Week",
+    };
+  },
+};
+</script>
+
+<style scoped></style>
